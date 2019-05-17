9b3df764046cfa5db080779d5732e873e57d33b1
mariansurdu112 <surdu_marian94@yahoo.com>
2019-05-17 18:02:09 +0300

    personal datas

--- MarAirCrewApp/src/app/pages/personal-docs/personal-docs.component.ts
+++ MarAirCrewApp/src/app/pages/personal-docs/personal-docs.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-personal-docs',
-  templateUrl: './personal-docs.component.html',
-  styleUrls: ['./personal-docs.component.scss']
+    selector: 'app-personal-docs',
+    templateUrl: './personal-docs.component.html',
+    styleUrls: ['./personal-docs.component.scss']
 })
 export class PersonalDocsComponent implements OnInit {
 
-  constructor() { }
+    docs: Array<any> = [{
+        Id: 1, Name: 'ID', ExpiryDate: new Date(2019, 9, 5), Icon: 'card'
+    },
+        {
+            Id: 2, Name: 'Passport', ExpiryDate: new Date(2019, 12, 5),Icon: 'card'
+        },
+        {
+            Id: 3, Name: 'Flying License', ExpiryDate: new Date(2020, 2, 15)
+        },
+        {
+            Id: 4, Name: 'Medical License', ExpiryDate: new Date(2019, 9, 25)
+        },
+        {
+            Id: 5, Name: 'Radio License', ExpiryDate: new Date(2019, 11, 29)
+        },
+        {
+            Id: 6, Name: 'Employee contract', ExpiryDate: new Date(2099, 12, 31)
+        }];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
