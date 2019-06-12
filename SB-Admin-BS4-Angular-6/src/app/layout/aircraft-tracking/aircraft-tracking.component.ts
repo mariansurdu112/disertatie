61976bb28771c3d102101e043a61b928f2ad3a71
mariansurdu112 <surdu_marian94@yahoo.com>
2019-06-12 18:33:34 +0300

    flight track complete

--- SB-Admin-BS4-Angular-6/src/app/layout/aircraft-tracking/aircraft-tracking.component.ts
+++ SB-Admin-BS4-Angular-6/src/app/layout/aircraft-tracking/aircraft-tracking.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
-import {AircraftTrackingService} from './aircraft-tracking.service';
+import { AircraftTrackingService } from './aircraft-tracking.service';
 
 @Component({
   selector: 'app-aircraft-tracking',
@@ -14,9 +14,10 @@ export class AircraftTrackingComponent implements OnInit {
   // tslint:disable-next-line:max-line-length
   data: any = {
     FlightTime: '4 h 40 min', Departure: new Date(), Arrival: new Date(new Date().getTime() + 3600 * 2000),
-    ActualDeparture: new Date(), ActualArrival: new Date(new Date().getTime() + 3600 * 2100)};
- constructor(private aircraftTrackingService: AircraftTrackingService) {
-   setInterval(() => { this.getData(); }, 6000);
+    ActualDeparture: new Date(), ActualArrival: new Date(new Date().getTime() + 3600 * 2100)
+  };
+  constructor(private aircraftTrackingService: AircraftTrackingService) {
+    setInterval(() => { this.getData(); }, 2000);
   }
   lat = 49.96;
   lng = 39.07;
@@ -38,33 +39,38 @@ export class AircraftTrackingComponent implements OnInit {
       Callsign: 'BMS'
     };
     this.aircraftTrackingService.getData(data).subscribe((res) => {
-       const datax = JSON.parse(res);
-       console.log(datax);
+      const datax = JSON.parse(res);
+      console.log(datax);
       const properties = Object.getOwnPropertyNames(datax).filter((prop) => {
-          return !unusedProps.includes(prop);
+        return !unusedProps.includes(prop);
       });
       console.log(properties);
       properties.forEach((prp) => {
-         console.log(datax[prp]);
-         const plane = {
-           Latitude: datax[prp][1],
-           Longitude: datax[prp][2],
-           AircraftModel: datax[prp][8],
-           RegNumber: datax[prp][9],
-           From: datax[prp][11],
-           To: datax[prp][12],
-           FlightNumber: datax[prp][16],
-           Callsign: datax[prp][18],
-           Prop: prp
-         };
-         if (this.planes.includes(plane)) {
-           const index = this.planes.indexOf(pln => pln.Regnumber === plane.RegNumber && pln.FlightNumber === plane.FlightNumber);
-           this.planes[index].Latitude = plane.Latitude;
-           this.planes[index].Longitude = plane.Longitude;
-            console.log(index);
-         } else {
-           this.planes.push(plane);
-         }
+        console.log(datax[prp]);
+        const plane = {
+          Latitude: datax[prp][1],
+          Longitude: datax[prp][2],
+          AircraftModel: datax[prp][8],
+          RegNumber: datax[prp][9],
+          From: datax[prp][11],
+          To: datax[prp][12],
+          FlightNumber: datax[prp][16],
+          Callsign: datax[prp][18],
+          Prop: prp
+        };
+        const existingData = this.planes.filter((pl) => {
+          return pl.RegNumber === plane.RegNumber && pl.FlightNumber === plane.FlightNumber;
+        });
+
+        if (existingData.length !== 0) {
+          const index = this.planes.map((pll) => pll.FlightNumber).indexOf(plane.FlightNumber);
+          console.log(index);
+          this.planes[index].Latitude = plane.Latitude;
+          this.planes[index].Longitude = plane.Longitude;
+          console.log(index);
+        } else {
+          this.planes.push(plane);
+        }
         console.log(this.planes);
       });
     });
