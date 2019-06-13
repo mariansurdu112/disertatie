3d399ae96fa309fe0d43bf6697073c10740c9a81
mariansurdu112 <surdu_marian94@yahoo.com>
2019-06-13 12:48:24 +0300

    Modules Services

--- SB-Admin-BS4-Angular-6/src/app/layout/administration/administration.service.ts
+++ SB-Admin-BS4-Angular-6/src/app/layout/administration/administration.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdministrationService {
-
-  constructor() { }
+    host = environment.API_URL;
+    httpOptions = {
+        headers: new HttpHeaders(
+            {
+                'Content-Type': 'application/json',
+            })
+    };
+    constructor(private http: HttpClient) { }
+    // getData(bounds: any): Observable<any> {
+    //     return this.http.post(this.host, bounds);
+    // }
 }
--- SB-Admin-BS4-Angular-6/src/app/layout/schedule/schedule.service.ts
+++ SB-Admin-BS4-Angular-6/src/app/layout/schedule/schedule.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScheduleService {
 
-  constructor() { }
+    host = environment.API_URL;
+    httpOptions = {
+        headers: new HttpHeaders(
+            {
+                'Content-Type': 'application/json',
+            })
+    };
+    constructor(private http: HttpClient) { }
+    // getData(bounds: any): Observable<any> {
+    //     return this.http.post(this.host, bounds);
+    // }
 }
